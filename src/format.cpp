2cdb0b4f51856bd976223bc14bbe10a69a1f7db3
chainingless <chainingless@gmail.com>
2020-06-22 22:46:47 +0800

    feat: add ZeroPaddedNumberString and enable ElapsedTime function

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,24 @@
-#include <string>
-
 #include "format.h"
 
+#include <string>
+
 using std::string;
+using std::to_string;
+
+string ZeroPaddedNumberString(string string) {
+  if (string.size() < 2) {
+    string.insert(string.begin(), 2 - string.size(), '0');
+  }
+  return string;
+}
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  string hoursString = ZeroPaddedNumberString(to_string(seconds / (60 * 60)));
+  string minutesString = ZeroPaddedNumberString(to_string((seconds / 60) % 60));
+  string secondsString = ZeroPaddedNumberString(to_string(seconds % 60));
+  return hoursString + ":" + minutesString + ":" + secondsString;
+}
