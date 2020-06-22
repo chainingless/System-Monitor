fb88f3e9c87ef5891e7be2bde902b3f6b879fb15
chainingless <chainingless@gmail.com>
2020-06-22 22:53:42 +0800

    feat: make it usable on both linux and macOS platform

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include "linux_parser.h"
+#include "macos_parser.h"
 #include "ncurses_display.h"
 #include "system.h"
 
 int main() {
-  System system;
+#ifdef __linux__
+  LinuxParser parser = LinuxParser();
+#elif __APPLE__
+  MacosParser parser = MacosParser();
+#endif
+  System system = System(parser);
   NCursesDisplay::Display(system);
 }
